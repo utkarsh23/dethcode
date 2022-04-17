ef6142079bfeecda056e36b82b70e6981ea9ddcb
Utkarsh Patil <utkarsh2305@gmail.com>
2022-04-18 02:19:42 +0530

    Fetch source code for traits used

--- packages/stacks-viewer/src/explorer/api-types.ts
+++ packages/stacks-viewer/src/explorer/api-types.ts
@@ -8,14 +8,21 @@ export interface TransactionResponse {
   tx_id: string;
   tx_status: string;
   tx_type: string;
+  sender_address: string;
   smart_contract?: ContractInfo;
 }
 
 export interface ContractIdResponse {
   tx_id: string;
+  source_code: string;
 }
 
 export interface ContractInfo {
   source_code: string;
   contract_id: string;
+}
+
+export interface ContractList {
+  useTrait: string[];
+  implTrait: string[];
 }
--- packages/stacks-viewer/src/explorer/fetchFiles.ts
+++ packages/stacks-viewer/src/explorer/fetchFiles.ts
@@ -20,6 +20,61 @@ export function addClarityExtension(fileName: string) {
   return fileName + ".clar";
 }
 
+function getIndicesOf(str: string, searchStr: string) {
+  var searchStrLen = searchStr.length;
+  if (searchStrLen == 0) {
+      return [];
+  }
+  var startIndex = 0, index, indices = [];
+  while ((index = str.indexOf(searchStr, startIndex)) > -1) {
+      indices.push(index);
+      startIndex = index + searchStrLen;
+  }
+  return indices;
+}
+
+function getFuncStatements(source: string, funcName: string) {
+  let statements = [];
+  const useTraitIndices = getIndicesOf(source, funcName);
+  for (let index of useTraitIndices) {
+    let count = 0;
+    while ((index + count) < source.length) {
+      count++;
+      if (source[index + count] === ")" ) {
+        break
+      }
+    }
+    statements.push(source.slice(index, index + count).split(/(\s+)/));
+  }
+  return statements;
+}
+
+function getContracts(source: string, senderAddr: string): types.ContractList {
+  let useTraitContracts = [];
+  const useTraitStatements = getFuncStatements(source, "use-trait");
+  for (let statement of useTraitStatements) {
+    const contract = statement[4].split(".");
+    if (contract[0] === "") {
+      contract[0] = senderAddr;
+    } else {
+      contract[0] = contract[0].substring(1);
+    }
+    useTraitContracts.push(contract[0].concat(".", contract[1]));
+  }
+  let implTraitContracts = [];
+  const implTraitStatements = getFuncStatements(source, "impl-trait");
+  for (let statement of implTraitStatements) {
+    const contract = statement[2].split(".");
+    if (contract[0] === "") {
+      contract[0] = senderAddr;
+    } else {
+      contract[0] = contract[0].substring(1);
+    }
+    implTraitContracts.push(contract[0].concat(".", contract[1]));
+  }
+  return { useTrait: useTraitContracts, implTrait: implTraitContracts };
+}
+
 export async function fetchFiles(
   apiName: ApiName,
   transactionId: string,
@@ -56,6 +111,29 @@ export async function fetchFiles(
   let files: FileContents = {};
   let fileName = addClarityExtension(response.smart_contract.contract_id);
   files[fileName] = response.smart_contract.source_code;
+
+  const contractList = getContracts(
+    response.smart_contract.source_code,
+    response.sender_address
+  );
+
+  for (let contract of contractList.useTrait) {
+    const contractInfoUrl =
+      apiUrl
+      + "/extended/v1/contract/"
+      + contract;
+    const contractInfo = await fetch(contractInfoUrl) as types.ContractIdResponse;
+    files[addClarityExtension(`use-trait/${contract}`)] = contractInfo.source_code;
+  }
+
+  for (let contract of contractList.implTrait) {
+    const contractInfoUrl =
+      apiUrl
+      + "/extended/v1/contract/"
+      + contract;
+    const contractInfo = await fetch(contractInfoUrl) as types.ContractIdResponse;
+    files[addClarityExtension(`impl-trait/${contract}`)] = contractInfo.source_code;
+  }
 
   return { files, mainFile: fileName};
 }
